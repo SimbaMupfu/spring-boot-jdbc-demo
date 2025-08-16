d41bd87d0b733f96f4b13193e6f42036b33ab5d8
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-16 21:44:55 +0200

    Create an object of Employee

--- src/main/java/inc/sims/hustles/SpringJDBCDemo/SpringJdbcDemoApplication.java
+++ src/main/java/inc/sims/hustles/SpringJDBCDemo/SpringJdbcDemoApplication.java
@@ -1,13 +1,21 @@
 package inc.sims.hustles.SpringJDBCDemo;
 
+import inc.sims.hustles.SpringJDBCDemo.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcDemoApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SpringJdbcDemoApplication.class, args);
+
+		Employee employee1 = context.getBean(Employee.class);
+		employee1.setEmpID(101);
+		employee1.setEmpName("Simbarashe");
+		employee1.setDepartment("Mobile");
 	}
 
 }
